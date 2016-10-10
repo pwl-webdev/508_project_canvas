d7683d133d00985dc9c2d126bb73596ca384f5b5
Pwl Webdev <pwl.webdev@gmail.com>
2016-10-10 22:30:10 +0200

    bullets created and animated

--- assets/scripts/missile_command.js
+++ assets/scripts/missile_command.js
@@ -14,6 +14,9 @@ var cities = [];
 var citiesNum = 6;
 var rocketLaunchers = [];
 var rocketLaunchersNum = 3;
+var missiles = [];
+var missilesNum = 15;
+var velocity = 3;
 
 var gameLoop = function(){
 	window.requestAnimFrame(gameLoop);
@@ -33,7 +36,59 @@ var setup = function(){
 	cities.push({'ax': 290, 'ay': 475, 'height': 30, 'width': 20});
 	cities.push({'ax': 350, 'ay': 475, 'height': 30, 'width': 20});
 	cities.push({'ax': 410, 'ay': 475, 'height': 30, 'width': 20});
+
+	$('#game').click(function(event){
+		//console.log(event);
+		//var posX = $(this).position().left;
+		//var posY = $(this).position().top;
+		//console.log('posX '+posX+" posY "+posY);
+	    var rect = canvas.getBoundingClientRect();
+	    var x = event.clientX - rect.left;
+	    var y = event.clientY - rect.top;
+	    if (missilesNum > 0){
+	    	shoot(x,y);
+	    }
+	});
 };
+
+var shoot = function(x, y){
+	//choose start point
+	console.log("x: " + x + " y: " + y);
+	var startX;
+	var startY = rocketLaunchers[0].by;
+	if( rocketLaunchers.length > 1){
+		if( x < (rocketLaunchers[0].bx + rocketLaunchers[1].bx)/2){
+			startX  = rocketLaunchers[0].bx;
+		} else {
+			startX = rocketLaunchers[1].bx;
+		}
+	} 
+	if (rocketLaunchers.length > 2){
+		if( x < (rocketLaunchers[1].bx + rocketLaunchers[2].bx)/2){
+				if( x < (rocketLaunchers[0].bx + rocketLaunchers[1].bx)/2){
+					startX  = rocketLaunchers[0].bx;
+				} else {
+					startX = rocketLaunchers[1].bx;
+				}
+		} else{
+			startX = rocketLaunchers[2].bx;
+		}		
+	}
+	console.log("startx: "+startX+" starty: "+startY);
+	var difx = x - startX;
+	var dify = y - startY;
+
+	//var velx = Math.sqrt((Math.pow(difx,2))/(Math.pow(difx,2)+Math.pow(dify,2)))*velocity;
+	//var vely = Math.sqrt((Math.pow(dify,2))/(Math.pow(difx,2)+Math.pow(dify,2)))*velocity;
+
+	var velx = difx/(Math.abs(difx) + Math.abs(dify))*velocity;
+	var vely = dify/(Math.abs(difx) + Math.abs(dify))*velocity;
+
+	console.log("velx: "+velx+" vely: "+vely);
+
+	missiles.push({'startX':startX, 'startY':startY, 'velx':velx, 'vely':vely, 'x':startX, 'y':startY});
+	missilesNum -= 1;
+}
 
 var draw = function(){
 	context.fillStyle = "black";
@@ -52,10 +107,26 @@ var draw = function(){
 		context.lineTo(rocketLaunchers[i].cx,rocketLaunchers[i].cy);
 		context.fill();
 	}
-
+	//draw cities
 	for(var i = 0; i < cities.length; i++){
 		context.fillStyle = "blue";
 		context.fillRect(cities[i].ax, cities[i].ay, cities[i].height, cities[i].width);		
+	}
+	//draw missiles
+	for(var i = 0; i < missiles.length; i++){
+		if(missiles[i].x > canvas.width || missiles[i].x < 0 || missiles[i].y < 0){
+			missiles.splice(i, 1);
+		} else {
+			console.log("draw");
+			context.strokeStyle = "white";
+			context.beginPath();
+			context.moveTo(missiles[i].startX,missiles[i].startY);
+			context.lineTo(missiles[i].x,missiles[i].y);
+			context.stroke();
+
+			missiles[i].x += missiles[i].velx;
+			missiles[i].y += missiles[i].vely;
+		}
 	}
 }
 
